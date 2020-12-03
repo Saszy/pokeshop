8cd35225e6a17a7bb3026f2ab7a73050a1954f32
Sasmita Pradhan <sasmita007tm@gmail.com>
2020-12-03 08:27:32 -0500

    added Redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import products from '../products'
+import React,{useEffect} from 'react'
+import {useDispatch, useSelector} from  'react-redux'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
 const HomeScreen = () => {
+   
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const {loading, error, products} = productList
+  
+  useEffect(()=>{
+     dispatch(listProducts())
+    },[dispatch])
+
     return (
       <>
         <h1>Latest Products</h1>
+        {loading? ( 
+        <h2>Loading...</h2>) 
+        : error ? (
+        <h3>{error}</h3>)
+      :(
         <Row>
             {products.map(product =>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -13,6 +28,7 @@ const HomeScreen = () => {
                 </Col>
             ))}
         </Row>
+        )}
       </>
     )
 }
